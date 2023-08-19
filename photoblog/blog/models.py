97914ffe390109db4bc82324a0a5824fdf124db9
eppae <minjeian@naver.com>
2023-08-19 11:52:11 +0900

    blog -login.html add

--- photoblog/blog/models.py
+++ photoblog/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Post(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+    name = models.CharField(max_length=30)
+    birth = models.DateField(null=True,blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    email = models.EmailField(unique=True)
+    author =models.ForeignKey(User, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=40)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
--- photoblog/blog/views.py
+++ photoblog/blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponse
+from django.contrib import messages  #진행상태를 메시지로 폼에서 출력하고 싶을 때 사용
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+def login(request):
+    render(request,'account/auth-login-basic.html')
